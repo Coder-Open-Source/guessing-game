9fceaeb6c2f13287543c9923e5483ad82ef3516d
Fernando <fernando.amorim@st-one.io>
2023-11-15 16:08:38 -0300

    start project

---
+++ src/index.rs
@@ -0,0 +1,13 @@
+fn main(){
+    //Gerar um numero aleátorio entre 1 e 100.
+    //Solicitar ao usuario para digitar um palpite.
+    //Criar uma estimativa se o palpite esta perto ou longe.
+    //Se o palpite estiver correto:
+    // - O jogo imprimirá uma mensagem de "Parabéns".
+    //Se não:
+    // - O jogo irá dizer se o palpite está perto ou longe do numero aleatorio.
+    // - O jogo continuara para uma nova tentativa.
+
+    let mut x:i8;
+
+}
