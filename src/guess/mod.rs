f3dc70b8acb8e08081efd207fd3d86eb8e8b5627
jwcbmat <johnathan.wcb@gmail.com>
2023-11-19 01:15:53 -0300

    feat: guessing-game completed

--- src/guess/mod.rs
+++
@@ -1,9 +0,0 @@
-pub struct Test {
-    pub value: f32
-}
-
-impl Test {
-    pub fn print_log(&self, msg: &str) {
-        println!("{} {}", msg, self.value);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,52 @@
-mod number;
+use std::io;
+
+mod generator;
 
 fn main() {
-    let mut generator = number::Generator {
-        seed: 42
-    };
+    let instance_random = generator::Random; // instance_random module
+    let random_value = instance_random.get_value(); // get random value with rand lib
+    let mut user_attempt = 0;
+    let mut user_error = false; // user error control
 
-    generator.random();
+    loop {
+        let mut input = String::new(); // get user input value
+
+        if user_attempt == 0 {
+            println! ("Pick a number between 1 and 100 and see if you get it right!");
+        } else if user_error {
+            println! ("You typed a character outside the rules. Please choose another one.");
+        } else  {
+            println!("You messed up. Please try again.");
+        }
+
+        user_error = false; // reset error
+        user_attempt += 1;
+
+
+        io::stdin().read_line(&mut input).expect("Error reading input.");
+        
+        // check int type
+        match input.trim().parse::<i32>() {
+            Ok(value) => {
+                if value >= 100 {
+                    user_error = true;
+                    continue;
+                }
+
+                if value != random_value.try_into().unwrap() {
+                    continue;
+                }
+
+                println!(
+                    "Congratulations! You nailed it! The random number was: {}, you got it right on the {}° try",
+                    random_value,
+                    user_attempt
+                    );
+                break;
+            }
+            Err(_) => {
+                user_error = true;
+            }
+        }
+    }
 }
